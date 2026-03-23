fba0b62a36400935e497b29cb10d14a5069c943a
Chetan Bisht <chetanbisht6113@gmail.com>
2026-03-23 22:32:17 +0530

    -Changed the sameSite configuration from 'strict' to 'lax' for more flexible cookie extraction.

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         httpOnly: true,// Prevents client side javascript from accessing the cookie
         secure: process.env.NODE_ENV === "production", // Set secure flag in production
-        sameSite: "strict",// Prevents csrf attacks
+        sameSite: "",// Prevents csrf attacks
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
     return token;
