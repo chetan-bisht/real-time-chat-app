25af554fb294da4f7fa9857d426cda5ef285694a
Chetan Bisht <chetanbisht6113@gmail.com>
2026-03-24 22:59:23 +0530

    -Installed axios & zustand for api fetching and state management -Created multiple pages for the frontend -Added the navbar component file in the src/component folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-﻿import React from 'react'
-
+﻿import React from 'react';
+import Navbar from './components/Navbar';
+import {Route, Routes} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
+import SettingsPage from './pages/SettingsPage';
+import { useAuthStore } from './store/useAuthStore';
 
 const App = () => {
+  const { authUser } = useAuthStore();
   return (
-    <div>App</div>
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/signup' element={<SignUpPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+        <Route path='/settings' element={<SettingsPage/>} />
+      </Routes>
+
+    </div>
   )
 }
 
