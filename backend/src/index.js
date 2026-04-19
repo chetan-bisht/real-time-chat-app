acb0fedbfd0df5be6cafa4a7acbe9bcc8a0edb3c
Chetan Bisht <chetanbisht6113@gmail.com>
2026-04-19 17:39:19 +0530

    -Updated index.js for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,14 @@ import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 // Dotenv configuration
 dotenv.config();
 
 //Initialize app and port
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 // Middlewares and routes
 app.use(cors({
@@ -24,6 +26,15 @@ app.use(cookieParser());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// Serve static files from the React app in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Test route
 app.get("/", (req, res) => {
